04ae6a37f3391f3d1c41a519515c981a41a6d521
mostlysunnydps <maprudikova@gmail.com>
2022-04-14 22:36:54 +0200

    gradient

--- script.js
+++ script.js
@@ -1,43 +1,9 @@
-var sliderObjects = [];
-createSliderObjects();
+$(document).mousemove(function(event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
 
-function plusDivs(obj, n) {
-  var parentDiv = $(obj).parent();
-  var matchedDiv;
-  $.each(sliderObjects, function(i, item) {
-    if ($(parentDiv[0]).attr('id') == $(item).attr('id')) {
-      matchedDiv = item;
-      return false;
-    }
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+
+  $('.gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
   });
-  matchedDiv.slideIndex=matchedDiv.slideIndex+n;
-  showDivs(matchedDiv, matchedDiv.slideIndex);
-}
-
-function createSliderObjects() {
-  var sliderDivs = $('.slider');
-  $.each(sliderDivs, function(i, item) {
-    var obj = {};
-    obj.id = $(item).attr('id');
-    obj.divContent = item;
-    obj.slideIndex = 1;
-    obj.slideContents = $(item).find('.mySlides');
-    showDivs(obj, 1);
-    sliderObjects.push(obj);
-  });
-}
-
-function showDivs(divObject, n) {
-  debugger;
-  var i;
-  if (n > divObject.slideContents.length) {
-    divObject.slideIndex = 1
-  }
-  if (n < 1) {
-    divObject.slideIndex = divObject.slideContents.length
-  }
-  for (i = 0; i < divObject.slideContents.length; i++) {
-    divObject.slideContents[i].style.display = "none";
-  }
-  divObject.slideContents[divObject.slideIndex - 1].style.display = "block";
-}
